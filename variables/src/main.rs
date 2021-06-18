2f8173ca838d77a0f5cef60fa7b90c808ae2160f
coconan <coconan8@gmail.com>
2021-06-18 23:08:27 +0800

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,12 @@
-
-const MAX_POINTS: u32 = 100_00;
-
 fn main() {
-    let mut x = 5;
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
